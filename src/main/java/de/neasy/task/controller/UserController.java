fda97d5dcbc7b19fdcefae8e78c844d1eb6c9e60
Probir Biswas <engr.probir.cse@gmail.com>
2019-12-12 15:40:09 +0100

    validation

--- src/main/java/de/neasy/task/controller/UserController.java
+++ src/main/java/de/neasy/task/controller/UserController.java
@@ -2,14 +2,20 @@ package de.neasy.task.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("user/")
 public class UserController {
 
-    @GetMapping("register")
+    @GetMapping("/register")
     public String register() {
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String saveData() {
         return "register";
     }
 
